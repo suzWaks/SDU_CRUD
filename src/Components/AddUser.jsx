52cb07f089c17b65ffb2e3082bb0686c95719913
suzWaks <suzalwakhley@gmail.com>
2024-01-14 16:53:24 +0600

    Remove unused import statement in AddUser component

--- src/Components/AddUser.jsx
+++ src/Components/AddUser.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+
 export const AddUser = () => {
     const [firstName, setFirstName] = useState("");
     const [middleName, setMiddleName] = useState("");
