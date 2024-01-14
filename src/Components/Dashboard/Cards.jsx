12810a7ed744c7a42e628c79ba9e129fd1af15eb
suzWaks <suzalwakhley@gmail.com>
2024-01-14 18:33:47 +0600

    cards final

--- src/Components/Dashboard/Cards.jsx
+++ src/Components/Dashboard/Cards.jsx
@@ -45,24 +45,26 @@ export const Cards = () => {
 
     return (
         <div className='w-full pt-10 pb-5'>
-            <div className="flex flex-wrap justify-evenly space-x-4 m-4 overflow-x-auto">
-                {cardData.map((data, index) => (
-                    <div
-                        key={index}
-                        className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 transition duration-200 shadow-md ease-in-out transform hover:-translate-y-1 flex-grow p-6 border border-gray-200 rounded-lg hover:shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-4"
-                        style={{
-                            backgroundColor: data.bg,
-                        }}
-                    >
-                        <div>
-                            <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">{data.count}</h5>
-                            <p className="font-normal text-gray-200 dark:text-gray-400">{data.gender}</p>
-                        </div>
-                        <div className="ml-auto mt-2 w text-white ">{data.icon}</div>
-                    </div>
-                ))}
-            </div>
+          <div className="flex flex-wrap justify-evenly space-x-4 m-4 overflow-x-auto">
+            {cardData.map((data, index) => (
+              <div
+                key={index}
+                className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 transition duration-200 shadow-md ease-in-out transform hover:-translate-y-1 flex-grow p-6 border border-gray-200 rounded-lg hover:shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-4"
+                style={{
+                  backgroundColor: data.bg,
+                }}
+              >
+                <div className="flex justify-between">
+                  <div>
+                    <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">{data.count}1</h5>
+                    <p className="font-normal text-gray-200 dark:text-gray-400">{data.gender}</p>
+                  </div>
+                  <div className="ml-auto w text-white ">{data.icon}</div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    );
-
+      );
+      
 };
